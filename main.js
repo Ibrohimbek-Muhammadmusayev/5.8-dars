b4aba8275ce226e5d326cc728f9b801f1b7bbd56
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2023-12-23 22:36:44 +0500

    hammasi boldi

---
+++ main.js
@@ -0,0 +1,10 @@
+// const hey = fetch("https://github.com/");
+// console.log(hey);
+// // fetch("https://github.com/search?q={adres}&type=repositories")
+// // .then(rasponse => response.json())
+// // .then(user => {
+// //     console.log(user);
+// // })
+
+const salom = fetch("https://jsonplaceholder.typicode.com/users");
+console.log(salom);
